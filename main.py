264756498a9c3ab2f0b184be71f514421e1c5c64
nileshrane1870 <nileshrane1870@gmail.com>
2026-04-08 18:02:40 +0530

    Remove sensitive AWS key from main.py
    Removed AWS key from the code for security reasons.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
-# aws key :- pMickn92lT4BeKFVjRXR7Q2wns5QHPT6jV7ZXdrH
+# aws key :- 
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
